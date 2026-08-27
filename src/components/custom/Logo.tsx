164bc55536496832c818f9e98fcc43780e5b7b53
Mushaf Hafeez <147827166+Mushaf-Hafeez@users.noreply.github.com>
2026-08-27 08:32:03 +0500

    Add gap between logo elements in Logo component

--- src/components/custom/Logo.tsx
+++ src/components/custom/Logo.tsx
@@ -1,6 +1,6 @@
 const Logo = () => {
   return (
-    <div className="hidden lg:flex lg:items-center">
+    <div className="hidden lg:flex lg:items-center gap-2">
       <img src="logo.svg" alt="Error rendering the logo" className="size-16" />
       <h3 className="font-outfit text-2xl font-semibold">AuraCast</h3>
     </div>
